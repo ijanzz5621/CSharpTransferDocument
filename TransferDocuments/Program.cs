377582ff3c9814bdcce534cf7d2cb0f26181ccb9
Sharizan Redzuan <ijanzz5621@gmail.com>
2017-08-29 11:50:48 +0800

    add functions

--- TransferDocuments/Program.cs
+++ TransferDocuments/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,39 @@ namespace TransferDocuments
     {
         static void Main(string[] args)
         {
+            DataSet dsResult = new DataSet();
+
+            dsResult = GetData();
+
+            if (dsResult.Tables.Count > 0 && dsResult.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in dsResult.Tables[0].Rows)
+                {
+                    TransferDocuments(row);
+                    UpdateRecord(row);
+                }
+
+            }
+
         }
+
+        private static DataSet GetData()
+        {
+            DataSet dsResult = new DataSet();
+
+
+            return dsResult;
+        }
+
+        public static void TransferDocuments(DataRow row)
+        {
+
+        }
+
+        private static void UpdateRecord(DataRow row)
+        {
+
+        }
+
     }
 }
